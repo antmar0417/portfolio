ec5e1671797c857ad604fa6eec4c7eeca2af53f0
antmar0417 <tonyboy@live.se>
2022-02-11 10:51:32 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,5 +8,10 @@ add_action('after_setup_theme', function () {
 });
 
 add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com');
+    wp_enqueue_style('tailwindcss-typography', 'https://cdn.jsdelivr.net/npm/@tailwindcss/typography@0.5.x/dist/typography.min.css');
     wp_enqueue_style('style', get_stylesheet_uri());
 });
+
+require get_template_directory().'/post-types/project.php';
+require get_template_directory().'/taxonomies/tool.php';
